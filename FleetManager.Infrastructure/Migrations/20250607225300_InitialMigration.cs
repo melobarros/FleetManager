ac56c5246e6ac70c2f56f6354424f21e09f10427
Amauri Barros <36938889+melobarros@users.noreply.github.com>
2025-06-07 20:28:52 -0300

    Added Migrations

---
+++ FleetManager.Infrastructure/Migrations/20250607225300_InitialMigration.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FleetManager.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Vehicles",
+                columns: table => new
+                {
+                    Vehicle_ChassisSeries = table.Column<string>(type: "TEXT", nullable: false),
+                    Vehicle_ChassisNumber = table.Column<uint>(type: "INTEGER", nullable: false),
+                    ChassisSeries = table.Column<string>(type: "TEXT", nullable: false),
+                    ChassisNumber = table.Column<uint>(type: "INTEGER", nullable: false),
+                    Color = table.Column<string>(type: "TEXT", nullable: false),
+                    VehicleType = table.Column<string>(type: "TEXT", maxLength: 8, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vehicles", x => new { x.Vehicle_ChassisSeries, x.Vehicle_ChassisNumber });
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Vehicles");
+        }
+    }
+}
