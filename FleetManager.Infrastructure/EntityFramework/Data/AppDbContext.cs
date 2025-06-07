1fb8fcbbbcbd00e5ba668c0f998f8a8a20112430
Amauri Barros <36938889+melobarros@users.noreply.github.com>
2025-06-07 10:29:56 -0300

    Added DbContext configuration

---
+++ FleetManager.Infrastructure/EntityFramework/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using FleetManager.Domain.Entities;
+using FleetManager.Infrastructure.EntityFramework.Mappings;
+using Microsoft.EntityFrameworkCore;
+
+namespace FleetManager.Infrastructure.EntityFramework.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Vehicle> Vehicles { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) 
+            : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new VehicleMap());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ FleetManager.Infrastructure/EntityFramework/Mappings/VehicleMap.cs
@@ -0,0 +1,33 @@
+﻿using FleetManager.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FleetManager.Infrastructure.EntityFramework.Mappings
+{
+    public class VehicleMap :IEntityTypeConfiguration<Vehicle>
+    {
+        public void Configure(EntityTypeBuilder<Vehicle> builder)
+        {
+            builder.HasDiscriminator<string>("VehicleType")
+                .HasValue<Car>(nameof(Car))
+                .HasValue<Truck>(nameof(Truck))
+                .HasValue<Bus>(nameof(Bus));
+
+            builder.HasKey(b => new { b.ChassisId.Series, b.ChassisId.Number });
+
+            builder.OwnsOne(b => b.ChassisId, chassis =>
+                {
+                    chassis.Property(c => c.Series)
+                        .HasColumnName("ChassisSeries")
+                        .IsRequired();
+
+                    chassis.Property(c => c.Number)
+                        .HasColumnName("ChassisNumber")
+                        .IsRequired();
+                });
+
+            builder.Property(b => b.Color)
+                .IsRequired();
+        }
+    }
+}
