6de033f1b20b72c69532a018df104b41e5047039
Amauri Barros <36938889+melobarros@users.noreply.github.com>
2025-06-07 23:35:00 -0300

    Added VehicleFactoryTests

---
+++ FleetManager.Tests/Domain/VehicleFactoryTests.cs
@@ -0,0 +1,32 @@
+﻿using FleetManager.Domain.Entities;
+using FleetManager.Domain.Enums;
+using FleetManager.Domain.Factories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetManager.Tests.Domain
+{
+    public class VehicleFactoryTests
+    {
+        [Theory]
+        [InlineData(VehicleType.Car, typeof(Car))]
+        [InlineData(VehicleType.Truck, typeof(Truck))]
+        [InlineData(VehicleType.Bus, typeof(Bus))]
+        public void Create_ShouldReturnCorrectSubclass_ForEachVehicleType(VehicleType type, Type expectedType)
+        {
+            var vehicle = VehicleFactory.Create(type, "S1", 1, "Red");
+            Assert.IsType(expectedType, vehicle);
+        }
+
+        [Fact]
+        public void Create_ShouldThrowArgumentOutOfRangeException_OnUnknownVehicleType()
+        {
+            var invalidType = (VehicleType)999;
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+                VehicleFactory.Create(invalidType, "X", 1, "Blue"));
+        }
+    }
+}
