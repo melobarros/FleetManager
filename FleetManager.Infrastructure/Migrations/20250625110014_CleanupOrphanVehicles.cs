cd5424d80329b8606c830b6b1cc20428673aad1b
Amauri Barros <36938889+melobarros@users.noreply.github.com>
2025-06-25 08:01:35 -0300

    Added migration to clean orphan vehicles

---
+++ FleetManager.Infrastructure/Migrations/20250625110014_CleanupOrphanVehicles.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FleetManager.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CleanupOrphanVehicles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Vehicles WHERE ProtocolId IS NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
